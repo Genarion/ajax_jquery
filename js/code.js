f6520afa03a72de9dc76a704f53b752fea45d079
Mario <marioferrernieto@gmail.com>
2018-02-12 02:27:01 +0100

    first commit

---
+++ js/code.js
@@ -0,0 +1,41 @@
+{
+  let $htmlContent = $('#contenidos');
+  let $headers = $('#cabeceras');
+  let $states = $('#estados');
+  let $stateCode = $('#codigo');
+  let $url = $('#recurso');
+  let $submitRequest = $('#enviar');
+
+  const STATES = ['UNINITIALIZED', 'LOADING', 'LOADED', 'INTERACTIVE', 'COMPLETE'];
+
+  let getRequest = function(){
+    $request = $.ajax({
+      url: $url.val(),
+      method: 'GET',
+      cache: false,
+      dataType: 'html',
+      xhrFields: {
+        onreadystatechange: function(){
+          $states.append('<strong>' + STATES[this.readyState] + ':</strong> ' + this.readyState + ' <br/>');
+          $stateCode.append(this.status + ': ' + this.statusText + ' <br/>');
+        }
+      }
+    })
+    .fail(function( msg ){
+      $htmlContent.html('Error, la petición ha fallado');
+    })
+    .done(function( msg ) {
+      $headers.append( $request.getAllResponseHeaders() );
+      $htmlContent.text( msg );
+    });
+  };
+
+  $(function(){
+    $url.val(location.href);
+
+    $submitRequest.click( function(event){
+      event.preventDefault();
+      getRequest();
+    });
+  })
+}
